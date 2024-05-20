0cdd74ba1f58d5199dcbc35b0a184fa6f45b3109
muhsroom1324 <mushroom1324@naver.com>
2024-05-20 11:36:19 +0900

    feature: local test environment

--- local_test.sh
+++ local_test.sh
@@ -11,7 +11,16 @@ docker run -d -p 8000:8000 amazon/dynamodb-local -jar DynamoDBLocal.jar -inMemor
 aws dynamodb create-table \
     --table-name test_ddb \
     --attribute-definitions \
-        AttributeName=id,AttributeType=S \
-    --key-schema AttributeName=id,KeyType=HASH \
+        AttributeName=url,AttributeType=S \
+    --key-schema AttributeName=url,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
     --endpoint-url http://localhost:8000
+
+# delete table
+aws dynamodb delete-table \
+    --table-name test_ddb \
+    --endpoint-url http://localhost:8000
+
+# scan table
+aws dynamodb scan --table-name test_ddb --endpoint-url http://localhost:8000
+
